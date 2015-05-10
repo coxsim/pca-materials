8a5af74f8d5b12ec73358617c66fe3d7411b10bc
Simon Cox <simon.cox@gmail.com>
2015-05-10 21:59:27 +0800

    First autosave of draft implementation

--- main.py
+++ main.py
@@ -3,6 +3,7 @@ __author__ = 'simon'
 import os
 import sys
 import markdown
+import logging
 from flask import Flask
 from flask import render_template
 from flask import Markup
@@ -10,6 +11,14 @@ from flask import request
 
 app = Flask(__name__)
 app.debug = True
+
+
+@app.before_first_request
+def setup_logging():
+    if not app.debug:
+        # In production mode, add log handler to sys.stderr.
+        app.logger.addHandler(logging.StreamHandler())
+        app.logger.setLevel(logging.INFO)
 
 script_dir = os.path.dirname(sys.argv[0])
 markdown_dir = os.path.join(script_dir, "markdown")
@@ -26,7 +35,12 @@ def markdown_page(markdown_file, title):
 
     edit = request.args.get('edit')
 
-    return render_template("markdown.html", title=title, content=content, markdown_content=markdown_content, edit=edit)
+    return render_template("markdown.html",
+                           title=title,
+                           content=content,
+                           markdown_file=markdown_file,
+                           markdown_content=markdown_content,
+                           edit=edit)
 
 @app.route("/news-and-bio")
 def news_and_bio():
@@ -55,6 +69,17 @@ def expert_witness():
 @app.route("/contact")
 def contact():
     return markdown_page("contact.md", "Contact Details")
+
+@app.route("/save-draft", methods=['POST'])
+def save_draft():
+    markdown_file = request.form["markdown_file"]
+    markdown_content = request.form["markdown_content"]
+    #request.form['foo']
+    app.logger.info(markdown_file)
+    app.logger.info(markdown_content)
+
+    return ""
+
 
 if __name__ == "__main__":
     app.run()
