2a6c5c2ac77e25ea91812128809c8302a81d3ca3
Simon Cox <simon.cox@gmail.com>
2015-06-07 10:34:08 -0400

    Formatting

--- main.py
+++ main.py
@@ -13,8 +13,9 @@ from flask import redirect
 from flask.helpers import url_for
 
 app = Flask(__name__)
-app.secret_key = "12345" # for session cookies
+app.secret_key = "12345"  # for session cookies
 app.debug = True
+
 
 @app.before_first_request
 def setup_logging():
@@ -23,19 +24,16 @@ def setup_logging():
         app.logger.addHandler(logging.StreamHandler())
         app.logger.setLevel(logging.INFO)
 
+
 script_dir = os.path.dirname(sys.argv[0])
 markdown_dir = os.path.join(script_dir, "markdown")
-
-
-
-
 
 import datetime
 
 TIMESTAMP_FORMAT = "%Y%m%d_%H%M%S"
 
-class DocumentStore(object):
 
+class DocumentStore(object):
     def __init__(self, directory):
         self.directory = directory
 
@@ -59,7 +57,6 @@ class DocumentStore(object):
         for filename in os.listdir(key_history_dir):
             yield (filename, datetime.datetime.strptime(filename, TIMESTAMP_FORMAT))
 
-
     def get_history(self, key, history_key):
         filename = os.path.join(self.directory, "history", key, history_key)
         save_time = datetime.datetime.strptime(history_key, TIMESTAMP_FORMAT)
@@ -68,20 +65,16 @@ class DocumentStore(object):
             return (save_time, f.read())
 
 
-
-
 markdown_store = DocumentStore(markdown_dir)
-
-
 
 
 @app.route("/")
 def index():
     return render_template("index.html")
 
+
 @app.route("/login", methods=['GET', 'POST'])
 def login():
-
     if request.form:
         username = request.form["username"]
         session["username"] = username
@@ -92,17 +85,19 @@ def login():
 
     return render_template("login.html", message=message)
 
+
 @app.route("/logout")
 def logout():
-
     del session["username"]
     del session["is_admin"]
     message = "Logged out"
 
     return render_template("login.html", message=message)
 
+
 def is_admin(user):
     return user == "coxsim"
+
 
 def markdown_page(markdown_file, title):
     edit = request.args.get('edit')
@@ -124,29 +119,36 @@ def markdown_page(markdown_file, title):
                            history=None,
                            drafts=None)
 
+
 @app.route("/news-and-bio")
 def news_and_bio():
     return markdown_page("news_bio.md", "News & Bio")
+
 
 @app.route("/multilayer-polymer-constructs")
 def multilayer_polymer_constructs():
     return markdown_page("multipc.md", "Multilayer Polymer Constructs")
 
+
 @app.route("/multilayer-dies")
 def multilayer_dies():
     return markdown_page("multidd.md", "Multilayer Dieheads")
+
 
 @app.route("/monolayer-dies")
 def monolayer_dies():
     return markdown_page("monodd.md", "Monolayer Dieheads")
 
+
 @app.route("/commissioning")
 def commissioning():
     return markdown_page("commission.md", "Commissioning")
 
+
 @app.route("/expert-witness")
 def expert_witness():
     return markdown_page("expert_witness.md", "Expert Witness")
+
 
 @app.route("/contact")
 def contact():
@@ -170,17 +172,20 @@ def edit(markdown_file):
                            history=history,
                            drafts=drafts)
 
+
 @app.route("/markdown/<markdown_file>", methods=['POST'])
 def save(markdown_file):
     markdown_content = request.form["markdown_content"]
     markdown_store.save(markdown_file, markdown_content)
     return redirect(url_for("edit", markdown_file=markdown_file))
 
+
 @app.route("/markdown/<markdown_file>/draft", methods=['POST'])
 def save_draft(markdown_file):
     markdown_content = request.form["markdown_content"]
     markdown_store.save("%s.draft" % markdown_file, markdown_content)
     return ""
+
 
 @app.route("/markdown/<markdown_file>/<history_key>")
 def markdown_history(markdown_file, history_key):
@@ -196,6 +201,7 @@ def markdown_history(markdown_file, history_key):
                            edit=True,
                            history=history,
                            drafts=drafts)
+
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0")
